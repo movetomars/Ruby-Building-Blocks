e810e655e4391a3cbf58c388e4da239c8f56ecdb
Maia Petee <maiapetee@OS-X-Mavericks.local>
2015-11-03 22:12:59 -0700

    Problem 1

--- Problem1/caesarcipher.rb
+++ Problem1/caesarcipher.rb
@@ -1,25 +1,32 @@
 def caesarCipher(string, shift)
     
+    new = ''
+    
     string.to_s.each_char do |s|
         int = s.to_s.ord
         cap = 0
         
-        if s == /[A-Z]/
-            int = s.to_s.ord - 64
-            cap = 64
-        elsif s == /[a-z]/
-            int = s.to_s.ord - 96
-            cap = 96
-        end
+            if s.match(" ") || s.match(",")
+                int = s.to_s.ord - shift
+                elsif s == /[A-Z]/
+                int = s.to_s.ord - 64
+                cap = 64
+                elsif s == /[a-z]/
+                int = s.to_s.ord - 96
+                cap = 96
+            end
+    
         
-        new = (int + shift.to_i + cap).chr
-        print new.to_s
+        new += (int + shift.to_i + cap).chr
+        
     end
     
-end
+    print new.to_s
     
-    puts "What do you want to encrypt?"
-    string = gets.chomp
-    puts "And how would you like to shift it?"
-    shift = gets.chomp.to_i
-    puts caesarCipher(string, shift)
+end
+
+puts "What do you want to encrypt?"
+string = gets.chomp
+puts "And how would you like to shift it?"
+shift = gets.chomp.to_i
+puts caesarCipher(string, shift)
