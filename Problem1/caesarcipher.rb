22ae8369fb4a75cf88c7d48500d864c2d5c76110
Maia <maia.petee@gmail.com>
2015-11-04 19:20:58 -0700

    Simplify, add wrap feature; Problem 1 complete

--- Problem1/caesarcipher.rb
+++ Problem1/caesarcipher.rb
@@ -7,25 +7,33 @@ def caesarCipher(string, shift)
         cap = 0
         
         if s.match(" ") || s.match(",")
-            int = s.to_s.ord - shift
-        elsif s == /[A-Z]/
-            int = s.to_s.ord - 64
-            cap = 64
-        elsif s == /[a-z]/
-            int = s.to_s.ord - 96
-            cap = 96
+                int = s.to_s.ord - shift
+                elsif s == /[A-Z]/
+                int = s.to_s.ord - 64
+                cap = 64
+                elsif s == /[a-z]/
+                int = s.to_s.ord - 96
+                cap = 96
+        end
+    
+        rough = int + shift.to_i + cap
+        
+        if rough > 122 || (rough < 97 && rough > 90)
+            rough -= 26
         end
         
-        new += (int + shift.to_i + cap).chr
+        new += rough.chr
         
     end
     
     print new.to_s
-    
+
+
 end
-    
-    puts "What do you want to encrypt?"
-    string = gets.chomp
-    puts "And how would you like to shift it?"
-    shift = gets.chomp.to_i
-    puts caesarCipher(string, shift)
+
+
+puts "What do you want to encrypt?"
+string = gets.chomp
+puts "And how would you like to shift it?"
+shift = gets.chomp.to_i
+puts caesarCipher(string, shift)
