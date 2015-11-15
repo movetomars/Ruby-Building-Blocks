4a393e5727b850a932a9f66ebc21d16c268e7962
movetomars <maia.petee@gmail.com>
2015-11-15 14:04:56 -0700

    Problem 3 Framework

---
+++ Problem3/substrings.rb
@@ -0,0 +1,27 @@
+def substrings(source, dictionary)
+    
+    @counts = Hash.new(0)
+    
+    source.each do |s|
+        
+        dictionary.each do |d|
+            if s.include?(d)
+                @counts[d] += 1
+            end
+        end
+        
+    end
+    
+    @counts
+end
+
+puts "What words are contained within the words that we speak?"
+puts "Enter a word or a sentence that you want to take a closer look at."
+
+source = gets.chomp.gsub(/[^a-zA-Z ]/, '').split(" ")
+
+puts "Enter some words, separated by commas, that you want to search for."
+
+dictionary = gets.chomp.split(" ")
+
+puts substrings(source, dictionary)
